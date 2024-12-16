2e18a8d6dead502a63ead530108ab2da18fe002d
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-16 11:42:06 +0700

    menambah query untuk menghapus data mahasiswa

--- app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.pertemuan9.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.pertemuan9.data.entity.Mahasiswa
@@ -18,5 +19,8 @@ interface MahasiswaDao {
     //get Mahasiswa
     @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
     fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 }
 
