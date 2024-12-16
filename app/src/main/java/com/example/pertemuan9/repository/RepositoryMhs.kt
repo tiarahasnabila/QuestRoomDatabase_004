62ad3ba6a9f5476b0e2bf1fe0a4840b105b73844
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-16 11:48:57 +0700

    menambah fungsi untuk mengambil data mahasiswa, dan dan mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/pertemuan9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/repository/RepositoryMhs.kt
@@ -1,7 +1,12 @@
 package com.example.pertemuan9.repository
 
 import com.example.pertemuan9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
 }
