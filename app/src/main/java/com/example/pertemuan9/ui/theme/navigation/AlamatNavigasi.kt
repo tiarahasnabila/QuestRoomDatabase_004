432f60c79517cdbe3ec171dfe53ea493fdd2bcfd
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-16 12:10:34 +0700

    menambahkan objek destinasi navigasi untuk Home, Detail dan update parameter NIM

--- app/src/main/java/com/example/pertemuan9/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan9/ui/theme/navigation/AlamatNavigasi.kt
@@ -2,4 +2,20 @@ package com.example.pertemuan9.ui.theme.navigation
 
 interface AlamatNavigasi {
     val route:String
+
+    object DestinasiHome : AlamatNavigasi{
+        override val route = "Home"
+    }
+
+    object DestinasiDetail : AlamatNavigasi {
+        override val route = "detail"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM"
+    }
+
+    object DestinasiUpdate : AlamatNavigasi {
+        override val route = "update"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM}"
+    }
 }
