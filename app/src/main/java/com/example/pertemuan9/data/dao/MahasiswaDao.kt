983196278e023d304a75e77bb46df1781ef37781
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-16 11:38:57 +0700

    menambah query untuk mengambil semua data mahasiswa

--- app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
@@ -2,11 +2,19 @@ package com.example.pertemuan9.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.pertemuan9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //get All Mahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+
 }
 
